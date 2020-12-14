e25a1eb53cda1b2960a6acec221d9420011c8bfd
ayu <seriusman0@gmail.com>
2020-12-14 18:59:50 +0700

    perubahan transfer sedikit

--- html/member/transfer1.php
+++ html/member/transfer1.php
@@ -101,11 +101,11 @@
 
 
         <div class="col-xs-12">
-          <h3>Mountain</h3>
+          <h3>erupiahcompany@outlook.com</h3>
 
           <div class="row">
             <div class="col-xs-12 col-sm-7">
-              <p><small>Website created with <a href="http://www.mashup-template.com/" title="Create website with free html template">Mashup Template</a>/<a href="https://www.unsplash.com/" title="Beautiful Free Images">Unsplash</a></small>
+              <p><small>Copyright © E-Rupiah. All rights reserved <a href="http://www.mashup-template.com/" title="Create website with free html template"></a><a href="https://www.unsplash.com/" title="Beautiful Free Images"></a></small>
               </p>
             </div>
             <div class="col-xs-12 col-sm-5">
