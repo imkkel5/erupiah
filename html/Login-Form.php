2dc68815a4c82e51844c7e31c6b4c1dcccff5b5c
seriusman <imkkelompok55@gmail.com>
2020-12-15 02:52:19 +0700

    fix email

--- html/Login-Form.php
+++ html/Login-Form.php
@@ -5,6 +5,59 @@
 
 <head>
   <?php include "header.php" ?>
+  <style>
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+    }
+
+    /* The Modal (background) */
+    .modal {
+      display: none;
+      /* Hidden by default */
+      position: fixed;
+      /* Stay in place */
+      z-index: 1000;
+      /* Sit on top */
+      padding-top: 100px;
+      /* Location of the box */
+      left: 0;
+      top: 0;
+      width: 100%;
+      /* Full width */
+      height: 100%;
+      /* Full height */
+      overflow: auto;
+      /* Enable scroll if needed */
+      background-color: rgb(0, 0, 0);
+      /* Fallback color */
+      background-color: rgba(0, 0, 0, 0.4);
+      /* Black w/ opacity */
+    }
+
+    /* Modal Content */
+    .modal-content {
+      background-color: #fefefe;
+      margin: auto;
+      padding: 20px;
+      border: 1px solid #888;
+      width: 80%;
+    }
+
+    /* The Close Button */
+    .close {
+      color: #aaaaaa;
+      float: right;
+      font-size: 28px;
+      font-weight: bold;
+    }
+
+    .close:hover,
+    .close:focus {
+      color: #000;
+      text-decoration: none;
+      cursor: pointer;
+    }
+  </style>
 </head>
 
 <body>
@@ -50,7 +103,7 @@
                       </div>
 
                       <div>
-                        <a href=""  class="txt1" id="forgotPass" >
+                        <a href="#"  class="txt1" id="forgotPass" >
                           Forgot Password?
                         </a>
                       </div>
@@ -67,6 +120,25 @@
         </div>
       </div>
     </div> <!-- .bg-light -->
+
+
+    <div id="forgotPassword" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+      <form action="" id="formEmailRec">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email</label>
+          <input type="email" class="form-control" id="email" placeholder="type your old email here ">
+        </div>
+        <div>
+          <input type="submit" name="submit" value="Kirim" class="form-control btn-primary" id="submit" placeholder="Rp">
+        </div>
+      </form>
+    </div>
+
+  </div>
+
 
     <?php include "footer.php" ?>
 
@@ -97,12 +169,10 @@
       })
     </script>
     <script>
-    
-    <script>
     var forgotPass = document.getElementById("forgotPass");
-    forgotPass.onclick = function(event) {
-          alert("berhasil");
-        }
+    forgotPass.onclick = function() {
+      forgotPassword.style.display = "block";
+    }
 
     </script>
 
